6f28a4966f9901f7fdd395e7d09a397f3784e7d9
Miłosz Zajonc <Milosz.Zajonc@proficredit.pl>
2020-03-03 09:13:22 +0100

    file loading fix

--- core/fileLoader.js
+++ core/fileLoader.js
@@ -6,19 +6,24 @@ const fs = require("fs");
     @param filename Ścieżka pliku tekstowego z danymi wejściowymi
  */
 const getFile = (players, filename) => {
-  const file = fs
+  let file = fs
     .readFileSync(filename, "utf8", (err, data) => {
       return data;
     })
-    .split("\r\n");
+    .split("\n");
 
   for (let i = 0; i < file.length - 1; i = i + 2) {
+    file[i] = file[i].replace('\r','');
+    file[i+1] = file[i+1].replace('\r','');
+
     players.push({
       name: file[i],
       totalScore: 0,
       scores: file[i + 1].split(",").map(Number)
     });
   }
+
+  console.log(file)
 };
 
 module.exports = getFile;
